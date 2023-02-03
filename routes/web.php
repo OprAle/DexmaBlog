3ed4e06325f4030bd8da07d74dd8cf7987e59305
Federico Di Stefano <federico.distefano@dexma.it>
2023-02-03 17:15:54 +0100

    Rimozione import

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
